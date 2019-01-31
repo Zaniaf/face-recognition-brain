d403b75fcdb7661fa29c5d566464766338df7911
Zaniaf <nickelargy91@gmail.com>
2019-01-31 22:03:26 +0100

    1

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('https://https://blooming-river-97986.herokuapp.com/imageurl', {
+    fetch('https://blooming-river-97986.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
